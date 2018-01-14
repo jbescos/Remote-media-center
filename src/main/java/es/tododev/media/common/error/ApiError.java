3bc54ee362add65810437c4d405d7a71084451c1
jbescos <jorgebescos@gmail.com>
2018-01-14 17:49:02 +0100

    Errors handler

--- src/main/java/es/tododev/media/common/error/ApiError.java
+++ src/main/java/es/tododev/media/common/error/ApiError.java
@@ -1,26 +1,15 @@
 package es.tododev.media.common.error;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.http.HttpStatus;
 
 public class ApiError {
 
 	private HttpStatus status;
 	private String message;
-	private List<String> errors;
 
-	public ApiError(HttpStatus status, String message, List<String> errors) {
+	public ApiError(HttpStatus status, String message) {
 		this.status = status;
 		this.message = message;
-		this.errors = errors;
-	}
-
-	public ApiError(HttpStatus status, String message, String error) {
-		this.status = status;
-		this.message = message;
-		errors = Arrays.asList(error);
 	}
 
 	public HttpStatus getStatus() {
@@ -38,14 +27,5 @@ public class ApiError {
 	public void setMessage(String message) {
 		this.message = message;
 	}
-
-	public List<String> getErrors() {
-		return errors;
-	}
-
-	public void setErrors(List<String> errors) {
-		this.errors = errors;
-	}
-	
 	
 }
--- src/main/java/es/tododev/media/common/error/ExceptionsController.java
+++ src/main/java/es/tododev/media/common/error/ExceptionsController.java
@@ -4,24 +4,22 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@ControllerAdvice
 public class ExceptionsController {
 
 	private final static Logger log = LogManager.getLogger();
 	
 	@ExceptionHandler(Exception.class)
-	public ModelAndView handleError(HttpServletRequest req, Exception ex) {
-		log.error("Request: " + req.getRequestURL() + " raised " + ex);
+	public ResponseEntity<ApiError> handleError(HttpServletRequest req, Exception ex) {
+		log.error("Request: " + req.getRequestURL() + " raised ", ex);
 
-		ModelAndView mav = new ModelAndView();
-		mav.addObject("exception", ex);
-		mav.addObject("url", req.getRequestURL());
-		mav.setViewName("error");
-		return mav;
+		ApiError apiError = new ApiError(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+		return ResponseEntity.status(apiError.getStatus()).body(apiError);
 	}
 
 }
