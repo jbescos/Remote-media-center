d49bca5e04f17b2246e975749065e479f8dbec51
jbescos <jorgebescos@gmail.com>
2018-02-03 23:51:03 +0100

    Update unix script

--- scripts/unixCompileAndExecute.sh
+++ scripts/unixCompileAndExecute.sh
@@ -3,4 +3,4 @@
 export PATH=/usr/lib/x86_64-linux-gnu:$PATH
 cd ..
 mvn clean install
-java -jar -Djava.awt.headless=false target/Remote-media-center-0.0.1-SNAPSHOT.jar
+java -jar -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=8081 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.awt.headless=false target/Remote-media-center-0.1.1.jar
