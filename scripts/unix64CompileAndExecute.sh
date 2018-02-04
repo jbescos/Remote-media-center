9a305652dc915bacc4d4b2be582f469b6e09e034
jbescos <jorgebescos@gmail.com>
2018-02-04 21:35:22 +0100

    VLC works in linux

--- scripts/unix64CompileAndExecute.sh
+++ scripts/unix64CompileAndExecute.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PATH=/usr/lib/i386-linux-gnu/:$PATH
+export PATH=/usr/lib/x86_64-linux-gnu/:$PATH
 cd ..
 mvn clean install
 java -jar -DVLCJ_INITX=no -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=8081 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.awt.headless=false target/Remote-media-center-0.1.1.jar
