e2d26d194231dbccefd8595fcfd5f34fb8a730de
Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>
2024-01-10 16:24:35 +0100

    Remove unnecessary JVM args
    Signed-off-by: Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>

--- raspberrypiCompileAndExecute.sh
+++ raspberrypiCompileAndExecute.sh
@@ -6,4 +6,4 @@ export PATH=/usr/lib/i386-linux-gnu/:$PATH
 
 mvn clean install
 
-java -jar -DVLCJ_INITX=no -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=8081 -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -Djava.awt.headless=false target/Remote-media-center-1.0.jar
+java -jar -DVLCJ_INITX=no -Djava.awt.headless=false target/Remote-media-center-1.0.jar
